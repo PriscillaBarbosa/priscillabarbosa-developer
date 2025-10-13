46dd16df0e6d0ecc7aa46ef0ffa7e2cfbd1bf1ac
Priscilla Barbosa <priscilla.barbosa@outlook.com>
2025-10-12 21:49:06 -0300

    adicionando elementos ao hero

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,4 +3,35 @@ import { inicializarDarkMode } from "./modules/darkMode.js";
 document.addEventListener('DOMContentLoaded', () => {
     inicializarDarkMode();
     console.log('DOM carregado, Dark Mode inicializado.');
+
+    const titulo = document.querySelector('.hero-titulo');
+    
+    if (titulo) {
+        const textoOriginal = titulo.textContent;
+        const letras = textoOriginal.split('');
+
+        titulo.textContent = ''; 
+
+        letras.forEach(letra => {
+            
+            const span = document.createElement('span'); 
+            
+            span.textContent = letra; 
+            
+            if(letra === ' ') {
+                span.className = 'espaco';
+            }
+            
+            titulo.appendChild(span);
+        });
+
+        const spans = titulo.querySelectorAll('span');
+        let delay = 75; 
+
+        spans.forEach((span, index) => {
+            setTimeout(() => {
+                span.classList.add('visible');
+            }, index * delay);
+        });
+    }
 });
